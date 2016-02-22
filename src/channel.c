5c8139ba69bfd8853753745091f33ba14db8c1f9
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-02-22 16:36:06 +0100

    First implementation of the channel

--- src/channel.c
+++ src/channel.c
@@ -2,11 +2,61 @@
 
 #include "channel.h"
 
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
 
-typedef struct channel
+
+/// Linked list of threads
+struct thread_t
 {
-    /// @todo impléménter le channel
+    pthread_t id;
+    struct thread_t * next;
+};
 
-} channel_t;
+struct thread_queue_t
+{
+    struct thread_t * head;
+    struct thread_t * queue;
+};
+
+/// Linked list of data
+struct data_t
+{
+    void * data;
+    struct thread_t * next;
+};
+
+struct data_queue_t
+{
+    struct data_t * head;
+    struct data_t * queue;
+};
 
 
+/// Channel
+struct channel
+{
+    /// @todo implémenter le channel
+    int eltsize;                    // Size of an element
+    int size;                       // Number of elements
+    int closed;
+
+    struct data_queue_t dataq;      // Linked list of data
+    struct thread_queue_t recvq;    // Is that useful?
+    struct thread_queue_t sendq;    // Is that useful?
+
+    pthread_mutex_t lock;           // mutex for atomic operations
+};
+
+typedef struct channel channel_t;
+
+
+
+
+/*void main(int argc, char **argv)
+{
+
+
+    return 0;
+}*/
