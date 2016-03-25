cdb258504da266cd85330c82d81bbf47c87c0877
kenrab <Kenrabari@yahoo.fr>
2016-03-25 17:08:53 +0100

    add files cat_sock.c and cp_sock.c in directory process

---
+++ benchmark/process/cat_sock.c
@@ -0,0 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <sys/types.h>
+#include <pthread.h>
+
+#define BUFSIZE 1024
+#define FILENAMESIZE 256
+
+#define SOCK_PATH "/tmp/soquette"
+#define LISTEN_BACKLOG 2
+ 
+
+
+void * readfile(void* f){
+  int fd, r, s, c;
+  int buffer[BUFSIZE];
+  char file[FILENAMESIZE];
+  socklen_t sz;
+  struct sockaddr_un sun;
+
+  memset(buffer, 0, BUFSIZE);
+  s = socket(PF_UNIX, SOCK_STREAM, 0);
+  if(s < 0){
+    perror("socket");
+    pthread_exit(NULL);
+  }
+ 
+  memset(&sun, 0, sizeof(struct sockaddr_un));
+  sun.sun_family = AF_UNIX;
+  strncpy(sun.sun_path, SOCK_PATH, sizeof(sun.sun_path) - 1);
+  
+  usleep(1000);
+  sz = sizeof(struct sockaddr_un);
+  c = connect(s, (struct sockaddr *)&sun, sz);
+  if(c < 0){
+    perror("connect");
+    close(s);
+    pthread_exit(NULL);
+  }
+  
+  memcpy(file, f, FILENAMESIZE);
+    
+  fd = open(file, O_RDONLY);
+  if(fd < 0){
+    perror("open");
+    close(s);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  } 
+
+ 
+  while(( r = read(fd, buffer, BUFSIZE)) > 0){
+    send(s, buffer, r, MSG_NOSIGNAL);
+    memset(buffer, 0, BUFSIZE);
+  }
+  close(fd);
+  close(s);
+  pthread_exit(NULL);
+}
+
+void * printfile(void* f){
+  int s, bd, lt, ac, r;
+  int buffer[BUFSIZE];
+  socklen_t sz;
+  struct sockaddr_un sun;
+
+  memset(buffer, 0, BUFSIZE);
+
+  s = socket(PF_UNIX, SOCK_STREAM, 0);
+  if(s < 0){
+    perror("socket");
+    pthread_exit(NULL);
+  }
+
+  memset(&sun, 0, sizeof(struct sockaddr_un));
+  sun.sun_family = AF_UNIX;
+  strncpy(sun.sun_path, SOCK_PATH, sizeof(sun.sun_path) - 1);
+
+  sz = sizeof(struct sockaddr_un);
+  
+  bd = bind(s, (struct sockaddr *) &sun, sz);
+  if(bd < 0){
+    perror("bind");
+    close(s);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  }
+
+  lt = listen(s,LISTEN_BACKLOG);
+
+  if(lt < 0){
+    perror("listen");
+    close(s);
+    pthread_exit(NULL);
+  }
+  
+  ac = accept(s, (struct sockaddr *) &sun, &sz);
+  if (ac < 0){
+    perror("accept");
+    close(s);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  }
+
+  while((r = recv(ac, buffer, BUFSIZE, 0)) > 0){
+    write(STDOUT_FILENO, buffer, r);
+    memset(buffer, 0, BUFSIZE);
+  }
+  close(s);
+  unlink(SOCK_PATH);
+  pthread_exit(NULL);
+}
+
+int main(int argc, char ** argv)
+{
+  if(argc != 2){
+    printf("usage: %s <file_name>\n",argv[0]);
+    return EXIT_FAILURE;
+  }
+
+  pid_t pid1 = fork();
+  pid_t pid2 = fork();
+
+  if(pid1 < 0 || pid2 < 0){
+    perror("erreur pid\n");
+    return EXIT_FAILURE;
+  }
+
+  else if(pid1 == 0){
+    readfile(argv[1]);
+    _exit(0);
+  }
+
+  else if(pid2 == 0){
+    printfile(NULL);
+    _exit(0);
+  }
+  
+  return 0;
+}
---
+++ benchmark/process/cp_sock.c
@@ -0,0 +1,190 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/fcntl.h>
+
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#include <pthread.h>
+
+#define BUFSIZE 1024
+#define FILENAME_SIZE 256
+
+#define SOCK_PATH "/tmp/gumi.socket"
+#define LISTEN_BACKLOG 2
+
+
+void * readfile(void * ptr)
+{
+  int r, fd;
+  int sock, err;
+  socklen_t sz;
+  int buffer[BUFSIZE];
+  char file[FILENAME_SIZE];
+  struct sockaddr_un un;
+
+  memset(buffer,0,BUFSIZE);
+  sock = socket(PF_UNIX,SOCK_STREAM,0);
+
+  if(sock == -1)
+  {
+    perror("error socket");
+    pthread_exit(NULL);
+  }
+
+  // Configure the socket setting the unix socket address
+  memset(&un,0,sizeof(struct sockaddr_un));
+  un.sun_family = AF_UNIX;
+  strncpy(un.sun_path, SOCK_PATH, sizeof(un.sun_path) - 1);
+
+  usleep(1000);
+  sz = sizeof(struct sockaddr_un);
+  err = connect(sock,(struct sockaddr *) &un, sz);
+
+  if(err == -1)
+  {
+      perror("error connect");
+      close(sock);
+      pthread_exit(NULL);
+  }
+
+  // Read the file and send its content
+  memcpy(file,ptr,FILENAME_SIZE);
+  fd = open(file,O_RDONLY);
+
+  if(fd == -1)
+  {
+    perror("write - open");
+    close(sock);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  }
+
+  while((r = read(fd,buffer,BUFSIZE)) > 0)
+  {
+    send(sock,buffer,r,MSG_NOSIGNAL);
+    memset(buffer,0,BUFSIZE);
+  }
+
+  close(fd);
+  close(sock);
+  pthread_exit(NULL);
+}
+
+void * writefile(void * ptr)
+{
+  int sock, sockclt;
+  int err, r, fd;
+  socklen_t sz;
+  int buffer[BUFSIZE];
+  char file[FILENAME_SIZE];
+
+  struct sockaddr_un un;
+  struct sockaddr_un *clt;
+
+  memset(buffer,0,BUFSIZE);
+  sock = socket(AF_UNIX,SOCK_STREAM,0);
+
+  if(sock == -1)
+  {
+    perror("error socket");
+    pthread_exit(NULL);
+  }
+
+  // Configure the socket
+  memset(&un,0,sizeof(struct sockaddr_un));
+  un.sun_family = AF_UNIX;
+  strncpy(un.sun_path, SOCK_PATH, sizeof(un.sun_path) - 1);
+
+  printf("-> %s \n",un.sun_path);
+
+  /// Bind
+  err = bind(sock, (struct sockaddr *) &un, sizeof(struct sockaddr_un));
+
+  if(err == -1)
+  {
+    perror("error bind");
+    close(sock);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  }
+
+  /// Listen
+  err = listen(sock,LISTEN_BACKLOG);
+
+  if(err == -1)
+  {
+    perror("error listen");
+    close(sock);
+    pthread_exit(NULL);
+  }
+
+  /// Accept
+  sz = sizeof(struct sockaddr_un);
+  sockclt = accept(sock, (struct sockaddr *) &clt,&sz);
+  if (sockclt == -1)
+  {
+    perror("error accept");
+    close(sock);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  }
+
+  // file opening and writing
+  memcpy(file,ptr,FILENAME_SIZE);
+  fd = open(file,O_CREAT|O_TRUNC|O_WRONLY,0600);
+
+  if(fd == -1)
+  {
+    perror("write - open");
+    close(sock);
+    unlink(SOCK_PATH);
+    pthread_exit(NULL);
+  }
+
+  while((r = recv(sockclt,buffer,BUFSIZE,0)) > 0)
+  {
+    write(fd,buffer,r);
+    memset(buffer,0,BUFSIZE);
+  }
+
+  close(fd);
+  close(sock);
+  unlink(SOCK_PATH);
+  pthread_exit(NULL);
+}
+
+
+int main(int argc, char **argv)
+{
+  if(argc != 3)
+  {
+    printf("usage: %s <file_src> <file_dest>\n",argv[0]);
+    return EXIT_FAILURE;
+  }
+  
+  pid_t pid1 = fork();
+  pid_t pid2 = fork();
+  
+  if(pid1 < 0 || pid2 < 0){
+    perror("erreur pid\n");
+    return EXIT_FAILURE;
+  }
+
+  else if(pid1 == 0){
+    readfile(argv[1]);
+    _exit(0);
+  }
+
+  else if(pid2 == 0){
+    writefile(argv[2]);
+    _exit(0);
+  }
+  
+  return EXIT_SUCCESS;
+}
