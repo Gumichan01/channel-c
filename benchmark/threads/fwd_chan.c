546d6b79cdf1343c6bd462139a1b361e570c02ae
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-04-06 11:44:20 +0200

    fwd_chan.c : refactored the program.

--- benchmark/threads/fwd_chan.c
+++ benchmark/threads/fwd_chan.c
@@ -19,8 +19,8 @@
 
 typedef struct msg_t
 {
-  pid_t pid;
-  char content[SIZE_MSG];
+      pid_t pid;
+      char content[SIZE_MSG];
 
 }msg_t;
 
@@ -30,143 +30,137 @@ int n = 0;
 
 void * receive(void * ptr)
 {
-  int err;
-  msg_t tmp;
+    int err;
+    msg_t tmp;
 
-  while(1)
-  {
-    err = channel_recv(chan_r,&tmp);
-    if(err <= 0)
+    while(1)
     {
-      break;
+        err = channel_recv(chan_r,&tmp);
+        if(err <= 0)
+        {
+            break;
+        }
+        /*else if(err > 0)
+          printf("%s",tmp.content);*/
     }
-    /*else if(err > 0)
-      printf("%s",tmp.content);*/
-  }
 
-  pthread_exit(NULL);
+    pthread_exit(NULL);
 }
 
 void * sendm(void * ptr)
 {
-  int err, nb = 0;
-  msg_t tmp;
+    int err, nb = 0;
+    msg_t tmp;
 
-  tmp.pid = getpid();
-  sprintf(tmp.content," %ld @ %d !\n",pthread_self(),tmp.pid);
+    tmp.pid = getpid();
+    sprintf(tmp.content," %ld @ %d !\n",pthread_self(),tmp.pid);
 
-  while(nb < MAX_MSG)
-  {
-    err = channel_send(chan_s,&tmp);
-
-    if(err == -1)
+    while(nb < MAX_MSG)
     {
-      channel_close(chan_s);
-      break;
-    }
-    nb++;
-  }
+        err = channel_send(chan_s,&tmp);
 
-  pthread_exit(NULL);
+        if(err == -1)
+        {
+            channel_close(chan_s);
+            break;
+        }
+        nb++;
+    }
+
+    pthread_exit(NULL);
 }
 
 void * forward(void * ptr)
 {
-  int err;
-  msg_t tmp;
+    int err;
+    msg_t tmp;
 
-  while((err = channel_recv(chan_s,&tmp)) != 0)
-  {
-      if(err == -1)
-      {
-        perror("FWD - error channel_recv");
-        channel_close(chan_s);
-        channel_close(chan_r);
-        break;
-      }
+    while((err = channel_recv(chan_s,&tmp)) != 0)
+    {
+        if(err == -1)
+        {
+            perror("FWD - error channel_recv");
+            channel_close(chan_s);
+            channel_close(chan_r);
+            break;
+        }
 
-      err = channel_send(chan_r,&tmp);
+        err = channel_send(chan_r,&tmp);
 
-      if(err == -1)
-      {
+        if(err == -1)
+        {
         perror("FWD - error channel_send");
         channel_close(chan_s);
         channel_close(chan_r);
         break;
-      }
+        }
 
-      n += 1;
+        n += 1;
 
-      if(n >= MAX_FWD_MSG)
-      {
-        channel_close(chan_s);
-        channel_close(chan_r);
-        break;
-      }
-  }
+        if(n >= MAX_FWD_MSG)
+        {
+            channel_close(chan_s);
+            channel_close(chan_r);
+            break;
+        }
+    }
   pthread_exit(NULL);
 }
 
 
 int main(int argc, char **argv)
 {
-  int i, err;
-  int nb_writers, nb_readers;
-  pthread_t th;
-  pthread_t thr[MAX_THREADS];
-  pthread_t thw[MAX_THREADS];
+    int i, err;
+    int nb_writers, nb_readers;
+    pthread_t th;
+    pthread_t thr[MAX_THREADS];
+    pthread_t thw[MAX_THREADS];
 
-  if(argc != 3)
-  {
-    printf("usage : %s <nb_writers> <nb_readers>\n",argv[0]);
-    return EXIT_FAILURE;
-  }
+    if(argc != 3)
+    {
+        printf("usage : %s <nb_writers> <nb_readers>\n",argv[0]);
+        return EXIT_FAILURE;
+    }
 
-  srand(time(NULL));
-  nb_writers = atoi(argv[1]);
-  nb_readers = atoi(argv[2]);
-  chan_s = channel_create(sizeof(msg_t),NB_MSG,0);
-  chan_r = channel_create(sizeof(msg_t),NB_MSG,0);
+    srand(time(NULL));
+    nb_writers = atoi(argv[1]);
+    nb_readers = atoi(argv[2]);
+    chan_s = channel_create(sizeof(msg_t),NB_MSG,0);
+    chan_r = channel_create(sizeof(msg_t),NB_MSG,0);
 
-  // Create every threads
-  for(i = 0; i < nb_writers; i++)
-  {
-    err = pthread_create(&thw[i],NULL,sendm,NULL);
+    // Create every threads
+    for(i = 0; i < nb_writers; i++)
+    {
+        err = pthread_create(&thw[i],NULL,sendm,NULL);
 
-    if(err != 0)
-        perror("pthread_create");
-  }
+        if(err != 0)
+            perror("pthread_create");
+    }
 
-  for(i = 0; i < nb_readers; i++)
-  {
-    err =pthread_create(&thr[i],NULL,receive,NULL);
+    for(i = 0; i < nb_readers; i++)
+    {
+        pthread_create(&thr[i],NULL,receive,NULL);
+    }
 
-    if(err != 0)
-        perror("pthread_create");
-  }
+    pthread_create(&th,NULL,forward,NULL);
 
-  err = pthread_create(&th,NULL,forward,NULL);
+    printf("Working...\n");
 
-  if(err != 0)
-      perror("pthread_create");
+    // Wait for every threads
+    for(i = 0; i < nb_writers; i++)
+    {
+        pthread_join(thw[i],NULL);
+    }
 
-  printf("Working...\n");
+    for(i = 0; i < nb_readers; i++)
+    {
+        pthread_join(thr[i],NULL);
+    }
 
-  // Wait for every threads
-  for(i = 0; i < nb_writers; i++)
-  {
-    pthread_join(thw[i],NULL);
-  }
+    pthread_join(th,NULL);
+    channel_destroy(chan_s);
+    channel_destroy(chan_r);
+    printf("End of program\n");
 
-  for(i = 0; i < nb_readers; i++)
-  {
-    pthread_join(thr[i],NULL);
-  }
-
-  pthread_join(th,NULL);
-  channel_destroy(chan_s);
-  channel_destroy(chan_r);
-  printf("End of program\n");
-
-  return EXIT_SUCCESS;
+    return EXIT_SUCCESS;
 }
