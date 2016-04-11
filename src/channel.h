6da443e20da7d2c44ee89efd4b97e989248ea026
Gumichan01 <luxon.jean.pierre@gmail.com>
2016-04-11 16:05:03 +0200

    channel.h : Modified the value of the flags.

--- src/channel.h
+++ src/channel.h
@@ -5,12 +5,10 @@
 struct channel;
 
 /* flags */
-#define CHANNEL_PROCESS_SHARED      0x00000001
-#define CHANNEL_PROCESS_BATCH       0x00000010
-#define CHANNEL_PROCESS_PTR         0x00000100
-#define CHANNEL_PROCESS_LOCKFREE    0x00001000
-#define CHANNEL_PROCESS_NONBLOCK    0x00010000  // Non-blocking channel (bonus)
-#define CHANNEL_PROCESS_SCHEDULER   0x00100000  // channel with scheduling (bonus)
+#define CHANNEL_PROCESS_SHARED      1
+#define CHANNEL_PROCESS_BATCH       2
+#define CHANNEL_PROCESS_PTR         4
+#define CHANNEL_PROCESS_NONBLOCK    8      // Non-blocking channel (bonus)
 
 
 struct channel *channel_create(int eltsize, int size, int flags);
